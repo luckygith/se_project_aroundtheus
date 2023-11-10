ce70bff1d66894ffecf2d9bdfbc9980d620ac6a7
LCKYLN <luckysvilla.mor@gmail.com>
2023-11-09 21:20:48 -0500

    feat toggle state button and refactor

--- scripts/validation.js
+++ scripts/validation.js
@@ -12,7 +12,7 @@ function showInputError(
   const errorMessageElement = formElement.querySelector(
     `#${inputElement.id}-error`
   ); //"#" + inputElement.id + "-error"
-  inputElement.classList.add(inputErrorClass); // class added that akes it red - OBJECT DESTRUCTURING (options.inputErrorClass)
+  inputElement.classList.add(inputErrorClass); // class added that makes it red - OBJECT DESTRUCTURING (options.inputErrorClass)
   errorMessageElement.textContent = inputElement.validationMessage;
   errorMessageElement.classList.remove(errorClass); //fade in
 }
@@ -33,20 +33,42 @@ function hideInputError(
 
 function checkInputValidity(formElement, inputElement, options) {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, options);
+    return showInputError(formElement, inputElement, options);
+  }
+  hideInputError(formElement, inputElement, options);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputElement) => inputElement.validity.valid);
+}
+
+function disableButton(submitButton) {
+  submitButton.classList.add(".modal__button_disabled");
+  submitButton.disabled = true;
+}
+
+function enableButton(submitButton) {
+  submitButton.classList.remove(".modal__button_disabled");
+  submitButton.disabled = false;
+}
+
+function toggleButtonState(inputElements, submitButton) {
+  if (hasInvalidInput(inputElements)) {
+    disableButton(submitButton);
   } else {
-    hideInputError(formElement, inputElement, options);
+    disableButton(submitButton);
   }
 }
 
 function setEventListeners(formElement, options) {
   const { inputSelector } = options; //const inputSelector = options.inputSelector  OBJECT DESTRUCTURING; sets value to the property
-
+  const submitButton = formElement.querySelector(".modal__button");
   const inputElements = [...formElement.querySelectorAll(inputSelector)];
   inputElements.forEach((inputElement) => {
     inputElement.addEventListener("input", (e) => {
       // add event listener for each input key to be listened to for validation
       checkInputValidity(formElement, inputElement, options); //shows details such as true or false validity// add event listener for each input key to be listened to for validation
+      toggleButtonState(inputElements, submitButton, options);
     });
   });
 }
@@ -67,10 +89,10 @@ function enableValidation(options) {
 enableValidation({
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: "modal__button_disabled",
+  submitButtonSelector: ".modal__button modal__button_disabled",
+  inactiveButtonClass: ".modal__button_disabled",
   inputErrorClass: "modal__input_type_error",
-  errorClass: "modal__error_visible",
+  errorClass: ".modal__error_visible",
 });
 
 enableValidation(config);
